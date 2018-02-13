af7a7fe4c9596fd80d838d2e644db87ce1b65ec9
Phillip Maddux <phillip@signalsciences.com>
2018-02-13 10:20:47 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 
 setup(
     name="pysigsci",
-    version="0.0.2",
+    version="0.0.3",
     author="foospidy",
     description=("A python wrapper for the Signal Sciences API - "
                  "https://docs.signalsciences.net/api/"),
