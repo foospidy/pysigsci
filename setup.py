530ce12d773a16985d500dcf3a4e993a83fe9555
Phillip Maddux <phillip@foospidy.com>
2018-08-28 09:51:12 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 
 setup(
     name="pysigsci",
-    version="1.2.12",
+    version="1.3.0",
     author="foospidy",
     description=("A python wrapper for the Signal Sciences API - "
                  "https://docs.signalsciences.net/api/"),
@@ -27,6 +27,6 @@ setup(
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
     ],
-    install_requires=['requests', 'pyopenssl', 'deepdiff'],
+    install_requires=['requests', 'pyopenssl', 'deepdiff', 'gitpython'],
     scripts=['pysigsci/bin/pysigsci', 'pysigsci/bin/pysigscia'],
 )
