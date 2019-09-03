e7395c5db7ba98a9e535ae1dfddea88c72d0bc53
foospidy <phillip@foospidy.com>
2019-09-03 14:20:24 -0400

    fixed content-type not getting set properly

--- pysigsci/sigsciapi/sigsciapi.py
+++ pysigsci/sigsciapi/sigsciapi.py
@@ -46,11 +46,11 @@ class SigSciApi(object):
 
         if endpoint != self.ep_auth and self.bearer_token != None:
             headers["Authorization"] = "Bearer {}".format(self.bearer_token['token'])
-            headers["Content-Type"] = "application/json"
         elif endpoint != self.ep_auth:
             headers["X-Api-User"] = self.api_user
             headers['X-Api-Token'] = self.api_token
 
+        headers["Content-Type"] = "application/json"
         headers['User-Agent'] = 'pysigsci v' + pysigsci.VERSION
 
         if self.cookies is not None:
